695afb550df7974338a0c476387078c40e63ff04
ahkrisztian <lunarcsik.krisztian@gmail.com>
2022-09-27 14:27:54 +0200

    Deleted Counter, FetchData pages and links

---
+++ fbayBlazorUI/Pages/Create.cs
@@ -0,0 +1,17 @@
+﻿using fbayModels.DTOs.AdvertismentDTOs;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace fbayBlazorUI.Pages
+{
+    public partial class Create
+    {
+        [Inject]
+        private HttpClient Http { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Http.GetFromJsonAsync<List<CreateAdvertisementDTO>>("/api/Advertisement/CreateAdvertisement");
+        }
+    }
+}
